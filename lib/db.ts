c5ae9160a6924980a96c31a3f52ad5899162e4a9
Mendelson Aleixo <mendelson.aleixo@gmail.com>
2026-02-06 05:48:42 -0300

    adiciona o clerk

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,21 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+const connectionString = process.env.DATABASE_URL;
+
+const prismaClientSingleton = () => {
+  const adapter = new PrismaNeon({
+    connectionString: connectionString!,
+  });
+
+  return new PrismaClient({ adapter });
+};
+
+export const db = globalThis.prisma ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.prisma = db;
